7c44b631a6e53f344a646cf00820e304f0d96fc2
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2022-12-15 22:11:15 -0300

    Inicio do card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="flex w-full h-screen items-center">
+    <div className="flex flex-col w-full h-screen items-center justify-center">
       <form>
-        <input type="text" placeholder="Cidade"></input>
-        <button type="submit">Pesquisar</button>
+        <input 
+        type="text" 
+        placeholder="Cidade"
+        className="p-3 rounded-lg outline-none"
+        ></input>
+        <button 
+        type="submit"
+        className="bg-blue-500 p-3 rounded-lg ml-3 text-white font-bold"
+        >Pesquisar</button>
       </form>
+      <Card />
     </div>
   );
 }

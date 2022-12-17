213da14f21747d9d6418e3217f1d7025cb96c0a6
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2022-12-17 10:11:27 -0300

    estilização do card onde irá mostrar a temperatura e outras informações

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Card() {
   return (
-    <div>Card</div>
-  )
+    <div className="bg-white p-6 mt-10 rounded-lg drop-shadow-md"> 
+      <div className="text-center">
+        <span className="block text-xl font-bold text-slate-700">Fortaleza</span>
+        <span className="text-slate-400 text-sm font-medium">Ceará, Brasil</span>
+      </div>
+
+      <div className="flex mt-4 font-bold text-slate-700 mb-2">
+        <span className="text-7xl">27</span>
+        <span className="text-2xl mt-2">ºC</span>
+      </div>
+
+      <div className="text-center"> 
+        <span className="block">Icon</span>
+        <span className="text-slate-700 font-medium">Nublado hoje</span>
+      </div>
+    </div>
+  );
 }

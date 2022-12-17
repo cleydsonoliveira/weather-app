7ab9926aa9a4fd1c31f789bdaaa3e30d74bdd0a0
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2022-12-17 13:03:11 -0300

    inicio da configuração da api e de sua resposta àssincrona

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import React from "react";
+import fetchData from "./services/api";
 import Card from "./components/Card";
 
 function App() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchData().then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div className="flex flex-col w-full h-screen items-center justify-center">
-      <form>
-        <input 
-        type="text" 
-        placeholder="Cidade"
-        className="p-3 rounded-lg outline-none"
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Cidade"
+          className="p-3 rounded-lg outline-none"
         ></input>
-        <button 
-        type="submit"
-        className="bg-blue-500 p-3 rounded-lg ml-3 text-white font-bold"
-        >Pesquisar</button>
+        <button
+          type="submit"
+          className="bg-blue-500 p-3 rounded-lg ml-3 text-white font-bold"
+        >
+          Pesquisar
+        </button>
       </form>
       <Card />
     </div>
